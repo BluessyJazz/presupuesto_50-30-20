bd462b915e3280a5c152ab754bc0e3cccd9246ff
BluessyJazz <112785703+BluessyJazz@users.noreply.github.com>
2024-05-06 09:25:04 -0500

    Agregar documentación

--- src/budget_manager.py
+++ src/budget_manager.py
@@ -1,5 +1,29 @@
+"""
+Este módulo contiene la función `calcular_presupuesto` que calcula cuánto
+dinero se debe destinar a cada categoría de un presupuesto mensual.
+"""
+
+
 def calcular_presupuesto(ingreso, porcentaje_gastos_esenciales):
+    """
+    Calcula cuánto dinero se debe destinar a cada categoría de un presupuesto
+    mensual.
+
+    Args:
+        ingreso (float): Ingresos mensuales netos.
+        porcentaje_gastos_esenciales (int): Porcentaje de ingresos que se
+            destinarán a gastos esenciales.
+
+    Returns:
+        tuple: Una tupla con tres elementos que representan el dinero que se
+            debe destinar a gastos esenciales, deseos y ahorros,
+            respectivamente.
+    """
+
+    # Calcular necesidades
     necesidades = ingreso * (porcentaje_gastos_esenciales / 100)
+
+    # Calcular deseos y ahorros
     if porcentaje_gastos_esenciales == 50:
         deseos = ingreso * 0.3  # 30% para estilo de vida
         ahorros = ingreso * 0.2  # 20% para ahorros/inversiones
@@ -9,8 +33,5 @@ def calcular_presupuesto(ingreso, porcentaje_gastos_esenciales):
     else:
         deseos = (ingreso - necesidades) * 3/5
         ahorros = ingreso - necesidades - deseos
-
-    # Ajustar los porcentajes de deseos y ahorros si exceden el 100% junto con los gastos esenciales
-    total = necesidades + deseos + ahorros
 
     return necesidades, deseos, ahorros
---
+++ src/data_handler.py
@@ -0,0 +1,4 @@
+"""
+Este módulo va a contener la implementación para el manejo de datos de la
+aplicación. Exportar e importar datos de un archivo
+"""
